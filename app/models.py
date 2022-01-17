c3ab3983790530c55e99159ba28c63a55c8547f9
FrancisFlow <iamngigikaranja1@aol.com>
2022-01-17 23:53:39 +0300

    Created pitches class

--- app/models.py
+++ app/models.py
@@ -1,5 +1,30 @@
 from . import db
 
+
+
+class Pitches(db.Model):
+    __tablename__='pitches'
+
+    id = db.Column(db.Integer, primary_key = True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    description = db.Column(db.String(), index = True)
+    title = db.Column(db.String())
+    pitcher = db.Column(db.String())
+    category = db.Column(db.String(255), nullable=False)
+    comments = db.relationship('Comment',backref='pitch',lazy='dynamic')
+    upvotes = db.relationship('Upvote', backref = 'pitch', lazy = 'dynamic')
+    downvotes = db.relationship('Downvote', backref = 'pitch', lazy = 'dynamic')
+
+
+
+class Comment:
+    def __init__(self, title, comment):
+
+        self.title = title
+        self.comment = comment
+
+
+
 class User(db.Model):
 
     __tablename__ = 'users'
