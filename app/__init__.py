6509afb45d9323c757f1e4b6cda89c9a2fdb8273
FrancisFlow <iamngigikaranja1@aol.com>
2022-01-18 12:15:40 +0300

    Registered auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,19 +2,25 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+
 bootstrap = Bootstrap()
 
 #initializing flask extensions
 db=SQLAlchemy()
 
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
 
     app= Flask(__name__)
 
-#creating the app configurations
+    #creating the app configurations
 
     app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
     #Initializing flask extensions
 
     bootstrap.init_app(app)
@@ -25,5 +31,11 @@ def create_app(config_name):
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+
+    #registering auth blueprint
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
 
     return app
