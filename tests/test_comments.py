b79d6bbcf3b977ec7124d7fc78bbb718c9a10632
FrancisFlow <iamngigikaranja1@aol.com>
2022-01-18 10:51:06 +0300

    Created tests for Comment model

---
+++ tests/test_comments.py
@@ -0,0 +1,27 @@
+import unittest
+
+from app.models import Comment
+from app import db
+
+
+class CommentModelTest(unittest.TestCase):
+
+    def setUp(self):
+
+        self.new_comment = Comment(description = 'content')
+        db.session.add(self.newcomment)
+        db.session.commit()
+
+
+
+    def tearDown(self):
+        Comment.query.delete()
+        db.session.commit()
+
+
+    def test_save_comment(self):
+        self.new_comment.save_comment()
+        self.assertTrue(len(Comment.query.all()>0))
+    
+    def test_instance_variables(self):
+        self.assertEquals(self.new_comment.description, 'content')
