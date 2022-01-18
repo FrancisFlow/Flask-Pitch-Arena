fc6704328d2c205bbdc20b9b05915eab03a6d694
FrancisFlow <iamngigikaranja1@aol.com>
2022-01-18 21:22:10 +0300

    Updated database uri

--- config.py
+++ config.py
@@ -1,18 +1,30 @@
+import os
+
+from instance.config import SECRET_KEY
 class Config:
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://francis:MincedMeat2.2@localhost/pitcharena'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://francis:Master@Work@localhost/pitcharena'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
-
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://francis:Master@Work.2@localhost/pitcharena'
+
 
 class TestConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://francis:Master@Work.2@localhost/pitcharena'
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://francis:Master@Work.2@localhost/pitcharena'
+
 
     DEBUG = True
 
